a3ff10cb874cf145f31913f81fa29cc211998db5
Aztek-128 <124203485+Aztek-128@users.noreply.github.com>
2023-06-12 09:53:24 -0700

    yes

--- task1.py
+++ task1.py
@@ -7,9 +7,9 @@ rectangle object through.
 w = tk.Tk()
 w.geometry("925x475")
 w.attributes('-topmost',True)
+w.title("please kill me")
 c = tk.Canvas(height=475,width=900,bg="#ffdddd")
 c.pack()
-label = tk.Label(text = "penis")
 
 
 
@@ -17,23 +17,21 @@ wallCoords = []
 f = open('map1.txt')
 data = f.read()
 data = list(data)
+
+x = 0
+y = 0
+lines = 0
+
 for i in data:
-    ix = x * i[0]
-    ix2 = xi * i[1]
-    x = 0
-    y = 0
-    xi = x + 25
-    yi = y + 25    
     print(i)
     if i == "*":
-        wallCoords.append( c.create_rectangle(x,y,xi,yi,fill = "black") )
-
-
-
-
-label.pack()
-
-
+        wallCoords.append( c.create_rectangle(0 +(25 * x),0 +(25* y),25 +(25 * x),25 +(25 * y),fill = "black") )
+    x += 1
+    
+    if x == 30:
+        y += 1
+        x = 0
+        
 
 
 
--- task2.py
+++ task2.py
@@ -13,12 +13,49 @@ Legend:
 5 Swamp
 6 City
 """
+'''
+0000000000
+0661111223
+0661111233
+1111122333
+1111223334
+5551223344
+5511122223
+5551111222
+1155111222
+1111122222
+1111122266
+'''
+
+
 w = tk.Tk()
 w.geometry("925x475")
 w.attributes('-topmost',True)
 c = tk.Canvas(height=475,width=900,bg="#ffdddd")
 c.pack()
-f = open('map1.txt')
-
+water = tk.PhotoImage(file="assets/map.water.png")
+plains = tk.PhotoImage(file="assets/map.plains.png")
+forest = tk.PhotoImage(file="assets/map.forest.png")
+hills = tk.PhotoImage(file="assets/map.hills.png")
+mountains = tk.PhotoImage(file="assets/map.mountains.png")
+swamp = tk.PhotoImage(file="assets/map.swamp.png")
+city = tk.PhotoImage(file="assets/map.city.png")
+c.create_rectangle(20,20,30,30,fill='green')
+x = 0
+y = 0
+textures = []
+f = open('map2.txt')
+data = f.read()
+data = list(data)
+#textures.append(c.create_image(20,20,image = water))
+for i in data:
+    x+=1
+    print(i,end="")
+    if i  == "0":
+        print(i) 
+        c.create_image(20*x,20*y,image = water)
+    if i == "1":
+        c.create_image
+    
 
 w.mainloop()
